0db513c0e7cf7dda392eb212d60932c16afb8e6b
frankhn <harfrank2@gmail.com>
2021-04-11 00:56:36 +0200

    update file reference

--- poseEstimation.py
+++ poseEstimation.py
@@ -6,7 +6,7 @@ mpDraw = mp.solutions.drawing_utils
 mpPose = mp.solutions.pose
 pose = mpPose.Pose()
 
-cap = cv2.VideoCapture("assets/pose-1.mp4")
+cap = cv2.VideoCapture("assets/pose1.mp4")
 pTime = 0
 
 while True:
