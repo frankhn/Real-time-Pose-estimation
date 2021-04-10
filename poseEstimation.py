07af1f287db68162a19f26c15665ef7c3911e8cb
frankhn <harfrank2@gmail.com>
2021-04-10 23:33:01 +0200

    detect landmarks

--- poseEstimation.py
+++ poseEstimation.py
@@ -2,11 +2,23 @@ import cv2
 import time
 import mediapipe as mp
 
-cap = cv2.VideoCapture("assets/pose-2.mp4")
+mpDraw = mp.solutions.drawing_utils
+mpPose = mp.solutions.pose
+pose = mpPose.Pose()
+
+cap = cv2.VideoCapture("assets/pose-1.mp4")
 pTime = 0
 
 while True:
     success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = pose.process(imgRGB)
+    print(results.pose_landmarks)
+
+    if results.pose_landmarks:
+        mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+        for id, lm in enumerate(results.pose_landmarks.landmark):
+            h, w, c = img.shape
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
